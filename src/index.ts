c998964e77e1c4a1f0c2f27fa5b4df2fa8079b81
petethepossum <47347759+petethepossum@users.noreply.github.com>
2024-02-28 18:30:17 +1300

    Update index.ts: fix "unknown export error"
    Change the import in index.ts to accuratly reflect the export in server group.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Server from 'mysql2/typings/mysql/lib/Server';
 import { DatabaseManager } from './database/database';
 import RedisManager from './redis/redis';
-import { ServerGroup } from './redis/server/server_group';
+import { ServerGroupPrefix } from './redis/server/server_group';
 import { Config } from './utils/config';
 import { Webserver } from './webserver/webserver';
 
