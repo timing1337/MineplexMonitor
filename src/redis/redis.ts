c57ff008bad513e34f8a1533859d78fea7d61a25
timing1337 <timing@crepe.moe>
2024-03-04 21:13:50 +0700

    some silly errors

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,7 +1,8 @@
 import { Redis } from 'ioredis';
 import { Config } from '../utils/config';
 import Logger from '../utils/log';
-import { MinecraftServer, ServerGroup } from './minecraft_server_data';
+import { MinecraftServer } from './minecraft_server_data';
+import { ServerGroup } from './server_group';
 
 export default class RedisManager {
     public static instance: Redis;
@@ -21,6 +22,10 @@ export default class RedisManager {
         const serverGroups = await RedisManager.instance.smembers('servergroups');
         if (serverGroups.length === 0 || !serverGroups.includes('Lobby')) {
             RedisManager.logger.log('Missing Lobby group, adding....');
+            const lobby = new ServerGroup("Lobby", "Lobby", 25700);
+            lobby.arcadeGroup = false;
+            lobby.maxPlayers = 50;
+            RedisManager.registerServerGroup(lobby);
         }
         RedisManager.logger.log(`There are currently ${serverGroups.length} groups: ${serverGroups.join(', ')}`);
     }
@@ -54,7 +59,7 @@ export default class RedisManager {
 
         await RedisManager.instance.hsetnx(serverGroupKey, 'uptimes', group.uptimes);
 
-        await RedisManager.instance.hsetnx(serverGroupKey, 'arcadeGroup', group.arcadeGroup);
+        await RedisManager.instance.hsetnx(serverGroupKey, 'arcadeGroup', String(group.arcadeGroup));
 
         await RedisManager.instance.hsetnx(serverGroupKey, 'worldZip', group.worldZip);
         await RedisManager.instance.hsetnx(serverGroupKey, 'plugin', group.plugin);
@@ -62,36 +67,36 @@ export default class RedisManager {
 
         await RedisManager.instance.hsetnx(serverGroupKey, 'portSection', group.portSection);
 
-        await RedisManager.instance.hsetnx(serverGroupKey, 'pvp', group.pvp);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'tournament', group.tournament);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'tournamentPoints', group.tournamentPoints);
+        await RedisManager.instance.hsetnx(serverGroupKey, 'pvp', String(group.pvp));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'tournament', String(group.tournament));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'tournamentPoints', String(group.tournamentPoints));
 
-        await RedisManager.instance.hsetnx(serverGroupKey, 'teamRejoin', group.teamRejoin);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'teamAutoJoin', group.teamAutoJoin);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'teamForceBalance', group.teamForceBalance);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'gameAutoStart', group.gameAutoStart);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'gameTimeout', group.gameTimeout);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'gameVoting', group.gameVoting);
+        await RedisManager.instance.hsetnx(serverGroupKey, 'teamRejoin', String(group.teamRejoin));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'teamAutoJoin', String(group.teamAutoJoin));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'teamForceBalance', String(group.teamForceBalance));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'gameAutoStart', String(group.gameAutoStart));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'gameTimeout', String(group.gameTimeout));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'gameVoting', String(group.gameVoting));
 
-        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardGems', group.rewardGems);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardItems', group.rewardItems);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardStats', group.rewardStats);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardAchievements', group.rewardAchievements);
+        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardGems', String(group.rewardGems));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardItems', String(group.rewardItems));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardStats', String(group.rewardStats));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'rewardAchievements', String(group.rewardAchievements));
 
-        await RedisManager.instance.hsetnx(serverGroupKey, 'hotbarInventory', group.hotbarInventory);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'hotbarHubClock', group.hotbarHubClock);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'playerKickIdle', group.playerKickIdle);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'hardMaxPlayerCap', group.hardMaxPlayerCap);
+        await RedisManager.instance.hsetnx(serverGroupKey, 'hotbarInventory', String(group.hotbarInventory));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'hotbarHubClock', String(group.hotbarHubClock));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'playerKickIdle', String(group.playerKickIdle));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'hardMaxPlayerCap', String(group.hardMaxPlayerCap));
 
         await RedisManager.instance.hsetnx(serverGroupKey, 'games', group.games);
         await RedisManager.instance.hsetnx(serverGroupKey, 'modes', group.modes);
         await RedisManager.instance.hsetnx(serverGroupKey, 'boosterGroup', group.boosterGroup);
         await RedisManager.instance.hsetnx(serverGroupKey, 'serverType', group.serverType);
 
-        await RedisManager.instance.hsetnx(serverGroupKey, 'addNoCheat', group.addNoCheat);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'addWorldEdit', group.addWorldEdit);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'whitelist', group.whitelist);
-        await RedisManager.instance.hsetnx(serverGroupKey, 'staffOnly', group.staffOnly);
+        await RedisManager.instance.hsetnx(serverGroupKey, 'addNoCheat', String(group.addNoCheat));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'addWorldEdit', String(group.addWorldEdit));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'whitelist', String(group.whitelist));
+        await RedisManager.instance.hsetnx(serverGroupKey, 'staffOnly', String(group.staffOnly));
 
         await RedisManager.instance.hsetnx(serverGroupKey, 'resourcePack', group.resourcePack);
         await RedisManager.instance.hsetnx(serverGroupKey, 'npcName', group.npcName);
